03bf439df1670b7b44ef3cc84bf5538e9435c5e7
Puru <puru.hirave@gmail.com>
2018-07-31 22:19:42 +0530

    Changed raw ptrs to unique ptrs

--- Structural Patterns/Decorator/MomoDecorator.h
+++ Structural Patterns/Decorator/MomoDecorator.h
@@ -5,63 +5,63 @@
 
 #include "stdafx.h"
 #include "IMomos.h"
+#include <memory>
 #include <iostream>
 #include <string>
 using namespace std;
 
 class MomoDecorator : public IMomo {
 public:
-    MomoDecorator(IMomo* m) : mDecorator(m) {}
-    virtual void operator()() = 0;
-    virtual void addCost(int addCost) = 0;
-    IMomo* get() { return mDecorator; }
+	MomoDecorator(std::unique_ptr<IMomo> m) : mDecorator(std::move(m)) {}
+	virtual void operator()() = 0;
+	virtual void addCost(int addCost) = 0;
+	IMomo* get() { return mDecorator->get(); }
 protected:
-    MomoDecorator(const MomoDecorator& dec) : mDecorator(dec.mDecorator) {}
-private:
-    IMomo* mDecorator;
+	MomoDecorator(const MomoDecorator& dec) : mDecorator(dec.mDecorator->get()) {}
+	std::unique_ptr<IMomo> mDecorator;
 };
 
 class FriedMomo : public MomoDecorator {
 public:
-    FriedMomo(IMomo* m) : MomoDecorator(m) {}
-    ~FriedMomo() {}
-    void operator()() {
-        cout << " Shalow Fried ";
-        addCost(10);
-        (*get())();
-    }
-    void addCost(int cost) {
-        get()->addCost(cost);
-    };
+	FriedMomo(std::unique_ptr<IMomo> m) : MomoDecorator(std::move(m)) {}
+	~FriedMomo() {}
+	void operator()() {
+		cout << " Shalow Fried ";
+		addCost(10);
+		(*get())();
+	}
+	void addCost(int cost) { 
+		get()->addCost(cost);
+	};
 
 };
 
 class ShezvanMomo : public MomoDecorator {
 public:
-    ShezvanMomo(IMomo* m) : MomoDecorator(m) {}
-    ~ShezvanMomo() {}
-    void operator()() {
-        cout << " Decorated with special Sezvan sause and soup ";
-        addCost(10);
-        (*get())();
-    }
-    void addCost(int cost) {
-        get()->addCost(cost);
-    };
+	ShezvanMomo(std::unique_ptr<IMomo> m) : MomoDecorator(std::move(m)) {}
+	~ShezvanMomo() {}
+	void operator()() {
+		cout << " Decorated with special Sezvan sause and soup ";
+		addCost(10);
+		(*get())();
+	}
+	void addCost(int cost) { 
+		get()->addCost(cost); 
+	};
 };
 
 class ChocolateMomo : public MomoDecorator {
 public:
-    ChocolateMomo(IMomo* m) : MomoDecorator(m) {}
-    ~ChocolateMomo() {}
-    void operator()() {
-        cout << " Decorated with Chocolate sause ";
-        addCost(20);
-        (*get())();
-    }
-    void addCost(int cost) {
-        get()->addCost(cost);
-    };
+	ChocolateMomo(std::unique_ptr<IMomo> m) : MomoDecorator(std::move(m)) {}
+	~ChocolateMomo() {}
+	void operator()() {
+		cout << " Decorated with Chocolate sause ";
+		addCost(30);
+		(*get())();
+	}
+	void addCost(int cost) { 
+		get()->addCost(cost); 
+	};
 };
 
 #endif
